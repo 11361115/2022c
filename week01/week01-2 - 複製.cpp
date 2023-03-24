d06835ff76a681338fecaf02bc60e4afc17590a8
11361115 <11361115@me.mcu.edu.tw>
2023-03-25 00:59:57 +0800

    add week01

---
+++ week01/week01-2 - 複製.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"enter number";
+    int a;
+    cin>>a;
+    cout<<a<<"\n";
+    cout<<"enter number";
+    string b;
+    cin>>b;
+    cout<<b<<"\n";
+    return 0;
+}
---
+++ week01/week01-3 - 複製.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    cout<<"enter"\n";
+        char line[40];
+    scanf("%s",line);
+    std::cout<<"RESULT"<<line;
+}
